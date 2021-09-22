f64b5383b3775b90c5905f3bb766ca8e07ebdb14
Twinks <twinklegohilchauhan@outlook.com>
2021-09-22 17:26:02 +0530

    minimized the long written code with setAttribute() and getAttribute() method.

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -59,62 +59,28 @@
 const list = document.querySelector("#navbar__list");
 
 const array = [
-    {text:"Home", class:".main__hero",classForLI: "home-nav"},
-    {text:"About", class:".about",classForLI: "about-nav"},
-    {text:"Service", class:".service",classForLI: "service-nav"},
-    {text:"Manual", class:".manual",classForLI: "manual-nav"},
-    {text:"Contact", class:".contact",classForLI: "contact-nav"}
+    {text:"Home", class:".main__hero", classForLI: "home-nav", classLIwithDot: ".home-nav"},
+    {text:"About", class:".about", classForLI: "about-nav", classLIwithDot: ".about-nav"},
+    {text:"Service", class:".service", classForLI: "service-nav", classLIwithDot: ".service-nav"},
+    {text:"Manual", class:".manual", classForLI: "manual-nav", classLIwithDot: ".manual-nav"},
+    {text:"Contact", class:".contact", classForLI: "contact-nav", classLIwithDot: ".contact-nav"}
 ]
 
 for(i=0; i<array.length; i++){
-    const items = document.createElement("li");
-    items.innerHTML = array[i]['text'];
-    list.appendChild(items);
+    const item = document.createElement("li");
+    item.innerHTML = array[i]['text'];
+    list.appendChild(item);
 
 
-    items.setAttribute("class", array[i]['classForLI']);
-
-    // items.addEventListener("click", function(evt){
-    //         const classes = document.querySelector(array[i]['class']);
-    //     classes.scrollIntoView();
-    // })
+    item.setAttribute("data-class", array[i]['class']);
 
     
+    item.addEventListener("click", function(evt){
+        const clickedLI = evt.target;
+        const section = clickedLI.getAttribute("data-class");
+        const sectToMove = document.querySelector(section);
+        sectToMove.scrollIntoView({behavior: "smooth"})
+    })
 }
 
-//code to move to the appropriate section from the navBar.
 
-const navigation = document.querySelector(".home-nav");
-navigation.addEventListener("click", function(){
-    console.log("Home-Page");
-    const homepage = document.querySelector(".main__hero");
-homepage.scrollIntoView();
-})
-
-const about = document.querySelector(".about-nav");
-about.addEventListener("click", function(){
-    console.log("About");
-    const aboutPage = document.querySelector("#section1");
-    aboutPage.scrollIntoView();
-})
-
-const service = document.querySelector(".service-nav");
-service.addEventListener("click", function() {
-    console.log("Service");
-    const servicePage = document.querySelector("#section2");
-    servicePage.scrollIntoView();
-})
-
-const manual = document.querySelector(".manual-nav");
-manual.addEventListener("click", function() {
-    console.log("Manual");
-    const manualPage = document.querySelector("#section3");
-    manualPage.scrollIntoView();
-})
-
-const contact = document.querySelector(".contact-nav");
-contact.addEventListener("click", function(){
-    console.log("Contact");
-    const homepage = document.querySelector(".contact");
-homepage.scrollIntoView();
-})
