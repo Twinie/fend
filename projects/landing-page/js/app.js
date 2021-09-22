3ddf3f9eb243113cadf9c4035becf389c864136c
Twinks <twinklegohilchauhan@outlook.com>
2021-09-22 12:02:47 +0530

    added list-items from the javascript and navigating to the appropriate section from the Navbar as required.

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -55,3 +55,66 @@
 // Set sections as active
 
 
+
+const list = document.querySelector("#navbar__list");
+
+const array = [
+    {text:"Home", class:".main__hero",classForLI: "home-nav"},
+    {text:"About", class:".about",classForLI: "about-nav"},
+    {text:"Service", class:".service",classForLI: "service-nav"},
+    {text:"Manual", class:".manual",classForLI: "manual-nav"},
+    {text:"Contact", class:".contact",classForLI: "contact-nav"}
+]
+
+for(i=0; i<array.length; i++){
+    const items = document.createElement("li");
+    items.innerHTML = array[i]['text'];
+    list.appendChild(items);
+
+
+    items.setAttribute("class", array[i]['classForLI']);
+
+    // items.addEventListener("click", function(evt){
+    //         const classes = document.querySelector(array[i]['class']);
+    //     classes.scrollIntoView();
+    // })
+
+    
+}
+
+//code to move to the appropriate section from the navBar.
+
+const navigation = document.querySelector(".home-nav");
+navigation.addEventListener("click", function(){
+    console.log("Home-Page");
+    const homepage = document.querySelector(".main__hero");
+homepage.scrollIntoView();
+})
+
+const about = document.querySelector(".about-nav");
+about.addEventListener("click", function(){
+    console.log("About");
+    const aboutPage = document.querySelector("#section1");
+    aboutPage.scrollIntoView();
+})
+
+const service = document.querySelector(".service-nav");
+service.addEventListener("click", function() {
+    console.log("Service");
+    const servicePage = document.querySelector("#section2");
+    servicePage.scrollIntoView();
+})
+
+const manual = document.querySelector(".manual-nav");
+manual.addEventListener("click", function() {
+    console.log("Manual");
+    const manualPage = document.querySelector("#section3");
+    manualPage.scrollIntoView();
+})
+
+const contact = document.querySelector(".contact-nav");
+contact.addEventListener("click", function(){
+    console.log("Contact");
+    const homepage = document.querySelector(".contact");
+homepage.scrollIntoView();
+})
