32fc8a826d975b95b2f37ab48d3994e736c7fc83
Twinks <twinklegohilchauhan@outlook.com>
2021-09-28 16:38:17 +0530

    JavaScript file formatted.

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,42 +1,51 @@
+const list = document.querySelector("#navbar__list"); // <ul> selected to add List items into it.
 
-
-const list = document.querySelector("#navbar__list");                                // <ul> selected to add List items into it.
-
-const array = [
-    {text:"About", class:".about"},
-    {text:"Service", class:".service"},
-    {text:"Manual", class:".manual"},
-    {text:"Contact", class:".contact"}
+const array = [{
+        text: "About",
+        class: ".about"
+    },
+    {
+        text: "Service",
+        class: ".service"
+    },
+    {
+        text: "Manual",
+        class: ".manual"
+    },
+    {
+        text: "Contact",
+        class: ".contact"
+    }
 ]
 
-for(let i=0; i<array.length; i++){
+for (let i = 0; i < array.length; i++) {
     const item = document.createElement("li");
     item.innerHTML = array[i]['text'];
     list.appendChild(item);
 
+    // the data-class value is added to the list items, which is actually the class of the sections to move to on clicking the list items. 
+    const classForLI = item.setAttribute("data-class", array[i]['class']); 
 
-    const classForLI = item.setAttribute("data-class", array[i]['class']);           // the data-class value is added to the list items, which is actually the class of the sections to move to on clicking the list items. 
-
-    
-    item.addEventListener("click", function(evt){
+    item.addEventListener("click", function(evt) {
         const clickedLI = evt.target;
         const section = clickedLI.getAttribute("data-class");
-        const sectToMove = document.querySelector(section);                        // Here the above data class is assigned by document.querySelector.
-        sectToMove.scrollIntoView({behavior: "smooth"});
+        // Here the above data class is assigned by document.querySelector.
+        const sectToMove = document.querySelector(section); 
+        sectToMove.scrollIntoView({
+            behavior: "smooth"
+        });
 
-        const selectToRemoveClass = document.querySelectorAll("ul li");           // to remove the styling once the other item is clicked
-        for(let j=0; j<selectToRemoveClass.length; j++){
-        const removedClass = selectToRemoveClass[j].classList;
-        removedClass.remove("active-li");
-
-        console.log(removedClass);
+        // to remove the styling once the other item is clicked
+        const selectToRemoveClass = document.querySelectorAll("ul li"); 
+        for (let j = 0; j < selectToRemoveClass.length; j++) {
+            const removedClass = selectToRemoveClass[j].classList;
+            removedClass.remove("active-li");
         }
-    
 
 
-        const log = clickedLI.classList;                                          // to add styling to active states, class is added to LI.
+        // to add styling to active states, class is added to LI.
+        const log = clickedLI.classList; 
         log.add("active-li");
-        console.log(log);
     })
 
 }
@@ -45,29 +54,32 @@ const navbarListItems = document.querySelectorAll('ul li')
 const scrollItems = document.querySelectorAll("section");
 let lastClass;
 
-window.addEventListener("scroll", function(){
-    const mainHeroHeight = document.querySelector(".main__hero").offsetTop;        // offsetTop is used to take up the height from the beginning of the document till the next section starts.
-    let fromTop = window.pageYOffset + mainHeroHeight;                             //pageYOffset is used to know the height of the scrolled page from top.  
+window.addEventListener("scroll", function() {
+    // offsetTop is used to take up the height from the beginning of the document till the next section starts.
+    const mainHeroHeight = document.querySelector(".main__hero").offsetTop; 
+    //pageYOffset is used to know the height of the scrolled page from top. 
+    let fromTop = window.pageYOffset + mainHeroHeight;  
 
     let scrolledItemArray = [];
-    scrollItems.forEach(function(scrollItem){
-if (scrollItem.offsetTop < fromTop){
-    scrolledItemArray.push(scrollItem)
-}
+    scrollItems.forEach(function(scrollItem) {
+        if (scrollItem.offsetTop < fromTop) {
+            scrolledItemArray.push(scrollItem)
+        }
     })
 
     const lastScrolledItem = scrolledItemArray[scrolledItemArray.length - 1];
     let classOfScrollItem = lastScrolledItem ? lastScrolledItem.getAttribute("class") : "";
 
-    if(lastClass !== classOfScrollItem){
+    if (lastClass !== classOfScrollItem) {
         lastClass = classOfScrollItem;
 
-navbarListItems.forEach(function(li, index){                                         
-    li.classList.remove("active-li");
+        navbarListItems.forEach(function(li, index) {
+            li.classList.remove("active-li");
 
-    if(li.getAttribute("data-class") === `.${lastClass}`){
-    li.classList.add("active-li");                                                 // again the active-li is added as to highlight the list-item whose section is being viewed.
-    }
-})
+            if (li.getAttribute("data-class") === `.${lastClass}`) {
+                // again the active-li is added as to highlight the list-item whose section is being viewed.
+                li.classList.add("active-li"); 
+            }
+        })
     }
 })
